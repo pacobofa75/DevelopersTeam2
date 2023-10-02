577f4c6ee9e30d2635b3d4778592ca33dbce9add
pacobofa75 <pacobofa@gmail.com>
2023-10-02 23:43:10 +0200

    Añadido Tailwind a Lista Tareas

--- public/lista_tareas.php
+++ public/lista_tareas.php
@@ -2,34 +2,37 @@
 <html>
 <head>
     <title>Lista de Tareas</title>
+    <!-- Agrega el enlace al archivo de estilo de Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-    <h1>Lista de Tareas</h1>
+<body class="bg-gray-100 p-8">
+    <div class="max-w-md mx-auto bg-white rounded p-6 shadow-lg">
+        <h1 class="text-2xl font-bold mb-4">Lista de Tareas</h1>
 
-    <?php
-    // Cargar las tareas desde el archivo JSON
-    $jsonFile = 'tasks.json';
-    $tasks = [];
+        <?php
+        // Cargar las tareas desde el archivo JSON
+        $jsonFile = 'tasks.json';
+        $tasks = [];
 
-    if (file_exists($jsonFile)) {
-        $jsonContent = file_get_contents($jsonFile);
-        $tasks = json_decode($jsonContent, true);
-    }
-
-    // Mostrar las tareas
-    if (!empty($tasks)) {
-        echo '<ul>';
-        foreach ($tasks as $task) {
-            echo '<li>' . $task['title'] . '</li>';
+        if (file_exists($jsonFile)) {
+            $jsonContent = file_get_contents($jsonFile);
+            $tasks = json_decode($jsonContent, true);
         }
-        echo '</ul>';
-    } else {
-        echo 'No hay tareas disponibles.';
-    }
-    ?>
 
-    <br>
-    <a href="index.php">Volver</a>
+        // Mostrar las tareas
+        if (!empty($tasks)) {
+            echo '<ul class="list-disc pl-6">';
+            foreach ($tasks as $task) {
+                echo '<li class="mb-2">' . $task['title'] . '</li>';
+            }
+            echo '</ul>';
+        } else {
+            echo '<p>No hay tareas disponibles.</p>';
+        }
+        ?>
+
+        <br>
+        <a href="index.php" class="text-blue-500 hover:underline">Volver</a>
+    </div>
 </body>
 </html>
-
